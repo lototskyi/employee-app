5298d52b7de3151520c74c853e689edf89fb7fde
alex <o.lototskyi@gmail.com>
2022-10-17 11:38:44 +0300

    dynamic employee list

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,20 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map(item => {
+
+        const {id, ...itemProps} = item;
+
+        return (
+            <EmployeesListItem key={id} {...itemProps}/>
+        );
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     );
 }
